304d7c674d2c93e4726cb3c6c77020c0e394ed45
Ramkumar Pillai <psramkumar@gmail.com>
2013-04-11 20:56:02 -0500

    Add as initials

---
+++ src/main/java/org/spring/Activator.java
@@ -0,0 +1,12 @@
+package org.spring;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+public class Activator implements BundleActivator {
+    public void start(BundleContext context) throws Exception {
+        System.out.println("Hello world");
+    }
+    public void stop(BundleContext context) throws Exception {
+        System.out.println("Goodbye World");
+    }
+}
